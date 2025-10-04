d617453f98c5821ed7c8f5bd5fd8ebdcb4bf4dc7
ridzcoder01 <smtechofcmodz@outlook.com>
2025-10-04 06:17:44 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -75,7 +75,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "Nebula~" + string_session;
+                        let md = string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `*Hey there, NEBULA-MD User!* 👋🏻
 
